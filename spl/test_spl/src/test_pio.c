641bc25d3bbc416caffe564fda078b0cd61b410a
Stefaan De Smet <stefaandesmet2003@yahoo.co.uk>
2021-09-09 12:33:36 +0200

    update test_spl

--- spl/test_spl/src/test_pio.c
+++ spl/test_spl/src/test_pio.c
@@ -1,10 +1,19 @@
 //#include "stm8l.h"
 #include "stm8s.h"
 
+#ifdef STM8S_BLACK
 #define Led_Init GPIO_Init(GPIOE, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_FAST)
 #define Led_ON   GPIO_WriteHigh    (GPIOE,GPIO_PIN_5)
 #define Led_OFF  GPIO_WriteLow     (GPIOE,GPIO_PIN_5)
 #define Led_TOG  GPIO_WriteReverse (GPIOE,GPIO_PIN_5)
+#endif 
+
+#ifdef STM8S_BLUE
+#define Led_Init GPIO_Init(GPIOB, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_FAST)
+#define Led_ON   GPIO_WriteHigh    (GPIOB,GPIO_PIN_5)
+#define Led_OFF  GPIO_WriteLow     (GPIOB,GPIO_PIN_5)
+#define Led_TOG  GPIO_WriteReverse (GPIOB,GPIO_PIN_5)
+#endif 
 
 void main(void) 
 {
